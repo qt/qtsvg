088be6d8c08cfdf54af0a857f766c9632b14a30b
Sergio Ahumada <sergio.ahumada@digia.com>
2012-11-27 19:53:19 +0100

    Change Nokia email and url to Qt Project
    Change-Id: I082c5c71f368bedf135e626e47e84e6d282ab39a
    Reviewed-by: Lars Knoll <lars.knoll@digia.com>

--- examples/embedded/desktopservices/linktab.cpp
+++ examples/embedded/desktopservices/linktab.cpp
@@ -67,9 +67,9 @@ void LinkTab::populateListWidget()
 QUrl LinkTab::itemUrl(QListWidgetItem *item)
 {
     if (m_WebItem == item) {
-        return QUrl(tr("http://qt.nokia.com"));
+        return QUrl(tr("http://qt-project.org"));
     } else if (m_MailToItem == item) {
-        return QUrl(tr("mailto:qt-info@nokia.com?subject=Qt feedback&body=Hello"));
+        return QUrl(tr("mailto:noreply@qt-project.org?subject=Qt feedback&body=Hello"));
     } else {
         // We should never endup here
         Q_ASSERT(false);
