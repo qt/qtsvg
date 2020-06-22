bb2deb1f692ad775fb39d141b3cbd17b334793d5
Volker Hilsheimer <volker.hilsheimer@qt.io>
2020-06-22 09:26:54 +0200

    Fix build after removal of QStyleOption::init in qtbase
    Use QStyleOption::initFrom instead.
    
    Align with qtbase 26a226630443a20bb2d3015431c019614e33a061.
    
    Change-Id: I244f4c51733dd97df37f3577debde4086721ab7b
    Reviewed-by: Liang Qi <liang.qi@qt.io>

--- src/svgwidgets/qsvgwidget.cpp
+++ src/svgwidgets/qsvgwidget.cpp
@@ -145,7 +145,7 @@ void QSvgWidget::paintEvent(QPaintEvent *)
 {
     Q_D(QSvgWidget);
     QStyleOption opt;
-    opt.init(this);
+    opt.initFrom(this);
     QPainter p(this);
     style()->drawPrimitive(QStyle::PE_Widget, &opt, &p, this);
     d->renderer->render(&p);
