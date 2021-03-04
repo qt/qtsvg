9f7ccbfc68d20d0dc2ddc1e7dee5572dcf7dcd48
Allan Sandfeld Jensen <allan.jensen@qt.io>
2021-03-04 14:28:48 +0100

    Clamp parsed doubles to float representable values
    Parts of our rendering assumes incoming doubles can still be sane
    floats.
    
    Fixes: QTBUG-91507
    Change-Id: I7086a121e1b5ed47695a1251ea90e774dd8f148d
    Reviewed-by: Robert Löhning <robert.loehning@qt.io>
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>
    Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>
    (cherry picked from commit bfd6ee0d8cf34b63d32adf10ed93daa0086b359f)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/svg/qsvghandler.cpp
+++ src/svg/qsvghandler.cpp
@@ -680,7 +680,8 @@ static qreal toDouble(const QChar *&str)
             val = -val;
     } else {
         val = QByteArray::fromRawData(temp, pos).toDouble();
-        if (qFpClassify(val) != FP_NORMAL)
+        // Do not tolerate values too wild to be represented normally by floats
+        if (qFpClassify(float(val)) != FP_NORMAL)
             val = 0;
     }
     return val;
@@ -3036,6 +3037,8 @@ static QSvgStyleProperty *createRadialGradientNode(QSvgNode *node,
         ncy = toDouble(cy);
     if (!r.isEmpty())
         nr = toDouble(r);
+    if (nr < 0.5)
+        nr = 0.5;
 
     qreal nfx = ncx;
     if (!fx.isEmpty())
