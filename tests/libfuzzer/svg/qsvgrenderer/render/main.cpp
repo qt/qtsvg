357b3bb399f5bd408b92d7101c8b99d785b2173b
Robert Löhning <robert.loehning@qt.io>
2021-12-20 22:47:14 +0100

    Fuzzing: Discard logging output from QSvgRenderer
    It's pointless in a fuzzer and slows down execution.
    
    Change-Id: Ic88d957224777f240426fe2f78a0a1a641b4432c
    Reviewed-by: Albert Astals Cid <albert.astals.cid@kdab.com>

--- tests/libfuzzer/svg/qsvgrenderer/render/main.cpp
+++ tests/libfuzzer/svg/qsvgrenderer/render/main.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
 **
-** Copyright (C) 2019 The Qt Company Ltd.
+** Copyright (C) 2021 The Qt Company Ltd.
 ** Contact: https://www.qt.io/licensing/
 **
 ** This file is part of the test suite of the Qt Toolkit.
@@ -30,6 +30,11 @@
 #include <QImage>
 #include <QPainter>
 #include <QSvgRenderer>
+#include <QtGlobal>
+
+// silence warnings
+static QtMessageHandler mh = qInstallMessageHandler([](QtMsgType, const QMessageLogContext &,
+                                                       const QString &) {});
 
 extern "C" int LLVMFuzzerTestOneInput(const char *Data, size_t Size) {
     static int argc = 3;
