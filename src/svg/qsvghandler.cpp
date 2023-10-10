effc44495a33babd4cf7a2044123f420e6b3da1c
Paul Olav Tvete <paul.tvete@qt.io>
2023-10-10 10:14:22 +0200

    Fix nullptr dereference with invalid SVG
    Fixes: QTBUG-117944
    Pick-to: 6.5 6.2
    Change-Id: I9059dc28c750fc0585f1fb982152b211c323c6cd
    Reviewed-by: Eskil Abrahamsen Blomfeldt <eskil.abrahamsen-blomfeldt@qt.io>
    (cherry picked from commit edc8ca7f1e45302223b4b7962a57a30918f84c8d)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/svg/qsvghandler.cpp
+++ src/svg/qsvghandler.cpp
@@ -3606,6 +3606,8 @@ void QSvgHandler::init()
 
 static bool detectCycles(const QSvgNode *node, QList<const QSvgUse *> active = {})
 {
+    if (Q_UNLIKELY(!node))
+        return false;
     switch (node->type()) {
     case QSvgNode::DOC:
     case QSvgNode::G:
