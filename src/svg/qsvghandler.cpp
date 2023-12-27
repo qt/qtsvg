7371c9aec48cfe53b91d6d0e59c91c693ec0ae2c
Matthias Rauter <matthias.rauter@qt.io>
2023-12-27 13:28:19 +0100

    Fix a crash when SVG files are loaded recursively
    This bug was triggered by a test in the resvg:
    structure/image/recursive-2.svg
    
    The issue is solved by not loading SVG files in the image element.
    This is in line with SVG 1.2 Tiny: "SVG Tiny 1.2 does not allow an
    SVG document to be referenced by the 'image' element", see:
    https://www.w3.org/TR/SVGTiny12/struct.html#ImageElement
    
    The old behavior can be reproduced by enabling trusted source mode,
    currently only by available by setting the environment variable
    QT_SVG_ASSUME_TRUSTED_SOURCE. In that case, there is no protection
    against recursively loaded SVG files.
    
    Pick-to: 6.5
    Change-Id: I08a4842cd36d183448b1ad1c19fbfca6339a3dd6
    Reviewed-by: Eskil Abrahamsen Blomfeldt <eskil.abrahamsen-blomfeldt@qt.io>
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    (cherry picked from commit eb1ff0e19e846b5b220ca8e4128816b0d032fcf5)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 5e6539b8b2d51cf036a2963a75e63bb16b8a5cd5)

--- src/svg/qsvghandler.cpp
+++ src/svg/qsvghandler.cpp
@@ -27,6 +27,7 @@
 #include "qtransform.h"
 #include "qvarlengtharray.h"
 #include "private/qmath_p.h"
+#include "qimagereader.h"
 
 #include "float.h"
 #include <cmath>
@@ -2782,7 +2783,9 @@ static QSvgNode *createImageNode(QSvgNode *parent,
                 filename = info.absoluteDir().absoluteFilePath(filename);
             }
         }
-        image = QImage(filename);
+
+        if (handler->trustedSourceMode() || !QImageReader::imageFormat(filename).startsWith("svg"))
+            image = QImage(filename);
     }
 
     if (image.isNull()) {
