863929f44639203c6678ca0f1a16e69e2d941cf0
Eirik Aavitsland <eirik.aavitsland@qt.io>
2024-03-11 13:15:10 +0100

    Avoid runtime warnings about negative font sizes
    The calculations assume that fonts have a point size, and not a pixel
    size set. But if an svg was rendered to a paint device (eg. a widget)
    that had a pixel sized font, runtime warnings would be produced, since
    the point size in that case is reported as -1.
    Fix by ensuring the painter is initially reset to a default, point
    sized font.
    
    Fixes: QTBUG-122752
    Pick-to: 6.7 6.6 6.5 5.15
    Change-Id: I5cfbb95ac3f5ac1299ae8a111bf8278836bf397b
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    Reviewed-by: Eskil Abrahamsen Blomfeldt <eskil.abrahamsen-blomfeldt@qt.io>

--- src/svg/qsvgnode.cpp
+++ src/svg/qsvgnode.cpp
@@ -615,6 +615,10 @@ void QSvgNode::initPainter(QPainter *p)
     p->setBrush(Qt::black);
     p->setRenderHint(QPainter::Antialiasing);
     p->setRenderHint(QPainter::SmoothPixmapTransform);
+    QFont font(p->font());
+    if (font.pointSize() < 0)
+        font.setPointSizeF(font.pixelSize() * 72.0 / p->device()->logicalDpiY());
+    p->setFont(font);
 }
 
 bool QSvgNode::shouldDrawNode(QPainter *p, QSvgExtraStates &states) const
