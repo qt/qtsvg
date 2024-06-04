7c11f5916cd4bec9a6a95880c97dcedf4051a095
Volker Hilsheimer <volker.hilsheimer@qt.io>
2024-06-04 15:28:03 +0200

    Option enum: add trailing comma
    Amends 14fd15bb431039899a506e2792806e27a9858517.
    
    Pick-to: 6.8
    Change-Id: If513d3f257baf21f0997e43495d9050f535c2a73
    Reviewed-by: Hatem ElKharashy <hatem.elkharashy@qt.io>

--- src/svg/qtsvgglobal.h
+++ src/svg/qtsvgglobal.h
@@ -15,7 +15,7 @@ namespace QtSvg {
 enum Option : quint32 {
     NoOption           = 0x00,
     Tiny12FeaturesOnly = 0x01,
-    AssumeTrustedSource = 0x02
+    AssumeTrustedSource = 0x02,
 };
 Q_DECLARE_FLAGS(Options, Option)
 Q_DECLARE_OPERATORS_FOR_FLAGS(Options)
