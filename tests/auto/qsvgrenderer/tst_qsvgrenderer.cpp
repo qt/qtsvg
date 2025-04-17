2aa8a765c75618b26be3d6cb4da71b5fbb4206c1
Robert Löhning <robert.loehning@qt.io>
2025-04-17 23:21:56 +0200

    Add missing tests for empty rects
    Those led to divisions by zero before that was fixed in 78cbbc1.
    
    Credit to OSS-Fuzz which found them as issue 23588.
    
    Task-number: QTBUG-49160
    Change-Id: I63210104e5d9270f1037919f22a9ff1fa7c110c3
    Reviewed-by: Hatem ElKharashy <hatem.elkharashy@qt.io>
    (cherry picked from commit 5366939f9146cac993eb37e6f3dfddca4f6f9dc8)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit b116901f3387eedfa865b986341d9bb8fa06d821)
    (cherry picked from commit 30ec80ae3b2a4c49274de9ad117a298f20207b25)

--- tests/auto/qsvgrenderer/tst_qsvgrenderer.cpp
+++ tests/auto/qsvgrenderer/tst_qsvgrenderer.cpp
@@ -27,6 +27,8 @@ public:
 
 private slots:
     void getSetCheck();
+    void emptyRect_data();
+    void emptyRect();
     void inexistentUrl();
     void emptyUrl();
     void invalidUrl_data();
@@ -101,6 +103,29 @@ void tst_QSvgRenderer::getSetCheck()
     QCOMPARE(0, obj1.framesPerSecond()); // Can't have a negative framerate
     obj1.setFramesPerSecond(INT_MAX);
     QCOMPARE(INT_MAX, obj1.framesPerSecond());
+}
+
+void tst_QSvgRenderer::emptyRect_data()
+{
+    // Testing rects with zero width or height or no given value
+    // Those caused divisions by zero, e.g. QTBUG-49160 and oss-fuzz issue 23588
+    // UBSAN is required to see those divisions by zero
+    QTest::addColumn<QByteArray>("svg");
+    QTest::newRow("nothing") << QByteArray(R"(<svg><rect/></svg>)");
+    QTest::newRow("no width zero height") << QByteArray(R"(<svg><rect height="0"/></svg>)");
+    QTest::newRow("zero width no height") << QByteArray(R"(<svg><rect width="0"/></svg>)");
+    QTest::newRow("no width") << QByteArray(R"(<svg><rect height="1"/></svg>)");
+    QTest::newRow("no height") << QByteArray(R"(<svg><rect width="1"/></svg>)");
+    QTest::newRow("both zero") << QByteArray(R"(<svg><rect width="0" height="0"/></svg>)");
+    QTest::newRow("zero width") << QByteArray(R"(<svg><rect width="0" height="1"/></svg>)");
+    QTest::newRow("zero heigth") << QByteArray(R"(<svg><rect width="1" height="0"/></svg>)");
+}
+
+void tst_QSvgRenderer::emptyRect()
+{
+    QFETCH(QByteArray, svg);
+    QSvgRenderer renderer(svg);
+    QVERIFY(renderer.isValid());
 }
 
 void tst_QSvgRenderer::inexistentUrl()
