af708aecc377bc07ab6dec37d661a2b544849ed5
Robert Löhning <robert.loehning@qt.io>
2025-05-02 23:23:56 +0200

    Test parsing of malformed data url
    qDecodeDataUrl() called from createImageNode() had a bug which caused an
    assert when parsing the svg in this test. The issue was fixed by
    4d839093b480d30eef8a89c0f864ee3f340adaa1 in qtbase. The test shall make
    sure that QSvgRenderer warns about the invalid input.
    
    Amends b65af14a2599cd7ad155bd254db9e86d795505d3
    
    Pick-to: 6.5
    Change-Id: I35e226046061d2bb40cc5719d8a67f16ae34fe76
    Reviewed-by: Hatem ElKharashy <hatem.elkharashy@qt.io>
    (cherry picked from commit 2fbfed685f0fa234e37ebe4593d57a7e41930579)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit e12653626354e4cc49c70b8b9d5d5a2462ca4fbe)

--- tests/auto/qsvgrenderer/tst_qsvgrenderer.cpp
+++ tests/auto/qsvgrenderer/tst_qsvgrenderer.cpp
@@ -71,6 +71,7 @@ private slots:
     void oss_fuzz_42532991();
     void oss_fuzz_399769595();
     void imageRendering();
+    void imageMalformedDataUrl();
     void illegalAnimateTransform_data();
     void illegalAnimateTransform();
     void tSpanLineBreak();
@@ -1818,6 +1819,16 @@ void tst_QSvgRenderer::imageRendering() {
         p2.end();
         QCOMPARE(img1, img2);
     }
+}
+
+void tst_QSvgRenderer::imageMalformedDataUrl()
+{
+    // The input below triggered an assert in qDecodeDataUrl() which is used when creating svg
+    // nodes. That assert is fixed and tested in qtbase. Still, the input is invalid and should be
+    // treated as such. The test makes sure that QSvgRenderer properly warns about it.
+    QTest::ignoreMessage(QtWarningMsg, R"(Could not create image from "data:charset,")");
+    QVERIFY(QSvgRenderer().load(
+            QByteArray("<svg><image width=\"1\" height=\"1\" xlink:href=\"data:charset,\"/></svg>")));
 }
 
 void tst_QSvgRenderer::illegalAnimateTransform_data()
