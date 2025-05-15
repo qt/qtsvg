dcee421bf1f5c97709cdee4afe29abd2ebb7ffc5
Juha Vuolle <juha.vuolle@qt.io>
2025-05-15 16:40:45 +0300

    Fix tst_qsvgrenderer when cssparser is disabled
    Remove css style usage from opacity() as css is not the core tested
    functionality. Skip stylePropagation() if cssparser is disabled.
    
    Fixes: QTBUG-136918
    Pick-to: 6.8
    Change-Id: I0718fd82f44415dfeb072b661a6ba65d311cc147
    Reviewed-by: Matthias Rauter <matthias.rauter@qt.io>
    (cherry picked from commit 333ae371df05a022208fc5e82fbd101e2e029791)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- tests/auto/qsvgrenderer/tst_qsvgrenderer.cpp
+++ tests/auto/qsvgrenderer/tst_qsvgrenderer.cpp
@@ -518,6 +518,9 @@ void tst_QSvgRenderer::nestedQXmlStreamReader() const
 
 void tst_QSvgRenderer::stylePropagation() const
 {
+#if !QT_CONFIG(cssparser)
+    QSKIP("cssparser feature disabled, skipping test");
+#else
     QByteArray data(R"(<svg>
                       <g id="foo" style="fill:#ffff00;">
                         <g id="bar" style="fill:#ff00ff;">
@@ -561,6 +564,7 @@ void tst_QSvgRenderer::stylePropagation() const
 
     QCOMPARE(image1, image2);
     QCOMPARE(image1, image3);
+#endif // QT_CONFIG(cssparser)
 }
 
 static qreal transformNorm(const QTransform &m)
@@ -1082,20 +1086,20 @@ void tst_QSvgRenderer::opacity()
     const char *svg = R"svg(
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="-1 -1 37 37">
     <g transform="translate(0, 0)">
-        <rect style="fill:#808080" x="0" y="0" width="10" height="10" fill-opacity="0.5"/>
-        <rect style="fill:#808080" x="5" y="5" width="10" height="10" fill-opacity="0.5"/>
+        <rect fill="#808080" x="0" y="0" width="10" height="10" fill-opacity="0.5"/>
+        <rect fill="#808080" x="5" y="5" width="10" height="10" fill-opacity="0.5"/>
     </g>
     <g transform="translate(20, 0)" fill-opacity="0.5">
-        <rect style="fill:#808080" x="0" y="0" width="10" height="10"/>
-        <rect style="fill:#808080" x="5" y="5" width="10" height="10"/>
+        <rect fill="#808080" x="0" y="0" width="10" height="10"/>
+        <rect fill="#808080" x="5" y="5" width="10" height="10"/>
     </g>
     <g transform="translate(0, 20)">
-        <rect style="fill:#808080" x="0" y="0" width="10" height="10" opacity="0.5"/>
-        <rect style="fill:#808080" x="5" y="5" width="10" height="10" opacity="0.5"/>
+        <rect fill="#808080" x="0" y="0" width="10" height="10" opacity="0.5"/>
+        <rect fill="#808080" x="5" y="5" width="10" height="10" opacity="0.5"/>
     </g>
     <g transform="translate(20, 20)" opacity="0.5">
-        <rect style="fill:#808080" x="0" y="0" width="10" height="10"/>
-        <rect style="fill:#808080" x="5" y="5" width="10" height="10"/>
+        <rect fill="#808080" x="0" y="0" width="10" height="10"/>
+        <rect fill="#808080" x="5" y="5" width="10" height="10"/>
     </g>
     </svg>
     )svg";
