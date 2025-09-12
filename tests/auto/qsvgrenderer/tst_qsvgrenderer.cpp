7e8898903265d931df0aa54b3913f2c49d4d7bf2
Robert Löhning <robert.loehning@qt.io>
2025-09-12 21:22:58 +0200

    Don't create group nodes which will be deleted anyway
    The old code first created the nodes, then checked whether their parent
    element has the right type and deleted them if not. This was wasted
    effort and could also lead to dangling pointers.
    
    Instead, first check the parent's type and only create the node if that
    matches.
    
    Task-number: QTBUG-139961
    Pick-to: 6.10.0 6.10 6.9 6.8
    Change-Id: Ifa870efbd5f336b34b81aa09b6fe79fb7fc826b9
    Reviewed-by: Hatem ElKharashy <hatem.elkharashy@qt.io>

--- tests/auto/qsvgrenderer/tst_qsvgrenderer.cpp
+++ tests/auto/qsvgrenderer/tst_qsvgrenderer.cpp
@@ -81,6 +81,7 @@ private slots:
     void testSymbol();
     void testMarker();
     void testPatternElement();
+    void testMisplacedElement();
     void testCycles();
     void testFeFlood();
     void testFeOffset();
@@ -2131,6 +2132,31 @@ void tst_QSvgRenderer::testPatternElement()
     p.end();
 
     QCOMPARE(refImage, image);
+}
+
+void tst_QSvgRenderer::testMisplacedElement()
+{
+    // This input caused a QSvgPattern node to be created with a QSvgPatternStyle referencing to it.
+    // The code then detected that the <pattern> element is misplaced in the <text> element and
+    // deleted it. That left behind the QSvgPatternStyle pointing to the deleted QSvgPattern. That
+    // was reported when running the test with ASAN or UBSAN.
+    QByteArray svg(R"(<svg>
+                      <text><pattern id="ptn" width="4" height="4"/></text>
+                      <g fill="url(#ptn) "/>
+                      </svg>)");
+
+    QImage image(20, 20, QImage::Format_ARGB32_Premultiplied);
+    image.fill(Qt::green);
+    QImage refImage = image.copy();
+
+    QTest::ignoreMessage(QtWarningMsg, "<input>:2:68: Could not add child element to parent "
+                                       "element because the types are incorrect.");
+    QTest::ignoreMessage(QtWarningMsg, "<input>:4:28: Could not resolve property: #ptn");
+
+    QSvgRenderer renderer(svg);
+    QPainter painter(&image);
+    renderer.render(&painter);
+    QCOMPARE(image, refImage);
 }
 
 void tst_QSvgRenderer::testCycles()
