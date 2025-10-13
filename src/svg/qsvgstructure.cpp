08a6328863baf824523cc268e568b43949bde106
Robert Löhning <robert.loehning@qt.io>
2025-10-13 23:00:33 +0200

    Guard against empty image size in QSvgPattern::patternImage()
    ...to avoid a following division by zero in
    QSvgPattern::calculateAppliedTransform()
    
    Pick-to: 6.10 6.8
    Change-Id: I027efbf54961f9fbfffe49542bbad620dab492b0
    Reviewed-by: Hatem ElKharashy <hatem.elkharashy@qt.io>

--- src/svg/qsvgstructure.cpp
+++ src/svg/qsvgstructure.cpp
@@ -883,6 +883,8 @@ QImage QSvgPattern::patternImage(QPainter *p, QSvgExtraStates &states, const QSv
     QSize imageSize;
     imageSize.setWidth(qCeil(patternBoundingBox.width() * t.m11() * m_transform.m11()));
     imageSize.setHeight(qCeil(patternBoundingBox.height() * t.m22() * m_transform.m22()));
+    if (imageSize.isEmpty())
+        return QImage(); // Avoid division by zero in calculateAppliedTransform()
 
     calculateAppliedTransform(t, peBoundingBox, imageSize);
     if (document()->isCalculatingImplicitViewBox())
