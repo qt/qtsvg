a84a367a8d779b8aa4ba811ede1dff310d5091f7
Hatem ElKharashy <hatem.elkharashy@qt.io>
2025-10-27 10:39:19 +0200

    Fix gradient stops current color parsing
    After the refactor done to CSS parsing, current color for gradient
    stops was no longer checked and led to missing style of gradient
    stops.
    
    Fixes: QTBUG-141102
    Pick-to: 6.10
    Change-Id: I80385164b0c2eebbf18abe887468105d145f317f
    Reviewed-by: Eskil Abrahamsen Blomfeldt <eskil.abrahamsen-blomfeldt@qt.io>

--- src/svg/qsvghandler.cpp
+++ src/svg/qsvghandler.cpp
@@ -2924,6 +2924,9 @@ static bool parseStopNode(QSvgStyleProperty *parent,
     attrs.setAttributes(styleCssAttributes, handler);
 #endif
 
+    //TODO: Handle style parsing for gradients stop like the rest of the nodes.
+    parseColor(&dummy, attrs, handler);
+
     QSvgGradientStyle *gradientStyle =
         static_cast<QSvgGradientStyle*>(parent);
     QStringView colorStr    = attrs.stopColor;
