a1e8a37f80479866f2c3b97c1ab37fe5871b2bb9
Volker Hilsheimer <volker.hilsheimer@qt.io>
2026-01-30 10:24:37 +0100

    QUIP-23: add security header to remaining source files
    Amends b1191f515d6c27c7bdc0ae964523690d4405e6c6, which moved security
    critical parsing code to separate files. The remaining sources do not
    contain code that crosses a trust boundary, so the probability that it
    represents an exploitable vulnerability is low.
    
    Pick-to: 6.11 6.10
    Task-number: QTBUG-135776
    Change-Id: Ifb95df43426ac50ac32851bf71c92cc309b762a1
    Reviewed-by: Hatem ElKharashy <hatem.elkharashy@qt.io>

--- src/svg/qsvgutils_p.h
+++ src/svg/qsvgutils_p.h
@@ -1,5 +1,7 @@
 // Copyright (C) 2025 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
+
 
 #ifndef QSVGUTILS_P_H
 #define QSVGUTILS_P_H
--- src/svg/qtsvgglobal.h
+++ src/svg/qtsvgglobal.h
@@ -1,6 +1,8 @@
 // Copyright (C) 2016 Intel Corporation.
 // Copyright (C) 2023 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
+
 
 #ifndef QTSVGGLOBAL_H
 #define QTSVGGLOBAL_H
