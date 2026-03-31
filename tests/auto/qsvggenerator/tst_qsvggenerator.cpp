1c2cd3128e7f46f25636b34af61ea206e4a1449f
Robert Löhning <robert.loehning@qt.io>
2026-03-31 12:42:13 +0200

    Expect warnings in tst_qsvggenerator
    Change-Id: I0b69b7679f9570f89bb0d2e500eb3b82ac7a4e54
    Reviewed-by: Hatem ElKharashy <hatem.elkharashy@qt.io>
    (cherry picked from commit 90ede5d42647f3fc2682e74151f7a92cac9504e4)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- tests/auto/qsvggenerator/tst_qsvggenerator.cpp
+++ tests/auto/qsvggenerator/tst_qsvggenerator.cpp
@@ -183,7 +183,9 @@ void tst_QSvgGenerator::escapesDescription()
 
 void tst_QSvgGenerator::outputDevice()
 {
-    QString fileName = "outputDevice_output.svg";
+    const QString fileName = "outputDevice_output.svg";
+    const QByteArray notOpenWarning =
+            QString("QIODevice::write (QFile, \"%1\"): device not open").arg(fileName).toLatin1();
     QFile::remove(fileName);
 
     QFile file(fileName);
@@ -195,6 +197,7 @@ void tst_QSvgGenerator::outputDevice()
         QCOMPARE(generator.outputDevice(), (QIODevice *)&file);
 
         QPainter painter;
+        QTest::ignoreMessage(QtWarningMsg, notOpenWarning.constData());
         QVERIFY(painter.begin(&generator));
         QCOMPARE(file.openMode(), QIODevice::OpenMode(QIODevice::Text | QIODevice::WriteOnly));
         file.close();
@@ -208,6 +211,7 @@ void tst_QSvgGenerator::outputDevice()
         QCOMPARE(generator.outputDevice(), (QIODevice *)&file);
 
         QPainter painter;
+        QTest::ignoreMessage(QtWarningMsg, notOpenWarning.constData());
         QVERIFY(painter.begin(&generator));
         QCOMPARE(file.openMode(), QIODevice::OpenMode(QIODevice::WriteOnly));
         file.close();
@@ -222,6 +226,7 @@ void tst_QSvgGenerator::outputDevice()
 
         QPainter painter;
         QTest::ignoreMessage(QtWarningMsg, "QSvgPaintEngine::begin(), could not write to read-only output device: 'Unknown error'");
+        QTest::ignoreMessage(QtWarningMsg, "QPainter::begin(): Returned false");
         QVERIFY(!painter.begin(&generator));
         QCOMPARE(file.openMode(), QIODevice::OpenMode(QIODevice::ReadOnly));
         file.close();
