463abe07c8f654d5009e804044d1440152466765
Marc Mutz <marc.mutz@qt.io>
2026-04-29 09:20:21 +0200

    QSvgEasingInterface: add pure interface guard rails
    Disable copying and make the dtor out-of-line (even if empty).
    
    Both are must-haves for polymorphic classes: the former prevents
    slicing, not only for the interface, but for all derived classes, too,
    and the latter prevents duplicate vtables and all the issue that can
    come from it, fixing -Wweak-vtable as we go.
    
    Amends e21efa08f412753ebc9ff29f8f2c440486668138 (6.11).
    
    Task-number: QTBUG-45582
    Change-Id: I3a4b75d8260389ff0ffe3ecf7e7cd6757a0c42f3
    Reviewed-by: Hatem ElKharashy <hatem.elkharashy@qt.io>
    (cherry picked from commit f8d9fb20da6c1da6e04097178bac3ac92ac60b06)

---
+++ src/svg/animation/qsvgeasinginterface.cpp
@@ -0,0 +1,12 @@
+// Copyright (C) 2025 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:trivial-impl-only
+
+#include "qsvgeasinginterface_p.h"
+
+QT_BEGIN_NAMESPACE
+
+QSvgEasingInterface::~QSvgEasingInterface()
+    = default;
+
+QT_END_NAMESPACE
