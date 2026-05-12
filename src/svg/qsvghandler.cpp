25cae41692e3a8673f4492c3a8721782284c351f
Marc Mutz <marc.mutz@qt.io>
2026-05-12 10:18:08 +0200

    QSvgHandler: Extract Method parseFontStyle()
    The parseFont() function is way too large for proper analysis, so
    start to break it down into pieces that are much easier to check for
    correctness.
    
    Start at a random place, with font-style.
    
    Comparison with [1] then immediately shows that we're good (but v4
    will greatly extend this).
    
    Move the parse call to the front of the function, so we can
    eventually, when all are ported, fix the discrepancy between what is
    being checked for the early return at the start of the function
    (isEmpty()) vs.what each individual block inside the remainder of the
    function checks (isEmpty() || == inherit). As speculated in the Jira
    ticket, this could lead to resource over-consumption if an SVG file
    uses explicit inherit a lot, because we create empty filter objects in
    that case vs. none for empty attributes.
    
    Amends the start of the git history in this module.
    
    [1] https://www.w3.org/TR/2018/REC-css-fonts-3-20180920/#font-style-prop
    
    Pick-to: 6.11 6.8
    Task-number: QTBUG-146726
    Change-Id: Ib0a4b9ab7c60ffbf05f5dba6b014d4188731abd5
    Reviewed-by: Robert Löhning <robert.loehning@qt.io>
    Reviewed-by: Hatem ElKharashy <hatem.elkharashy@qt.io>
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>

--- src/svg/qsvghandler.cpp
+++ src/svg/qsvghandler.cpp
@@ -46,7 +46,12 @@ Q_LOGGING_CATEGORY(lcSvgHandler, "qt.svg")
 
 namespace {
 namespace tokens {
+// common
 constexpr auto inherit = "inherit"_L1;
+constexpr auto normal = "normal"_L1;
+// font-style
+constexpr auto italic = "italic"_L1;
+constexpr auto oblique = "oblique"_L1;
 } // namespace tokens
 } // unnamed namespace
 
@@ -901,11 +906,28 @@ static FontSizeSpec fontSizeSpec(QStringView spec)
     return FontSizeValue;
 }
 
+static std::optional<QFont::Style> parseFontStyle(QStringView s)
+{
+    // https://www.w3.org/TR/2018/REC-css-fonts-3-20180920/#font-style-prop
+    //   Value: normal | italic | oblique
+
+    if (s == tokens::normal)
+        return QFont::StyleNormal;
+    if (s == tokens::italic)
+        return QFont::StyleItalic;
+    if (s == tokens::oblique)
+        return QFont::StyleOblique;
+
+    return std::nullopt; // incl. empty and tokens::inherit
+}
+
 static void parseFont(QSvgNode *node,
                       const QSvgAttributes &attributes,
                       QSvgHandler *)
 {
-    if (attributes.fontFamily.isEmpty() && attributes.fontSize.isEmpty() && attributes.fontStyle.isEmpty() &&
+    auto parsedFontStyle = parseFontStyle(attributes.fontStyle);
+
+    if (attributes.fontFamily.isEmpty() && attributes.fontSize.isEmpty() && !parsedFontStyle &&
         attributes.fontWeight.isEmpty() && attributes.fontVariant.isEmpty() && attributes.textAnchor.isEmpty())
         return;
 
@@ -946,15 +968,8 @@ static void parseFont(QSvgNode *node,
         }
     }
 
-    if (!attributes.fontStyle.isEmpty() && attributes.fontStyle != tokens::inherit) {
-        if (attributes.fontStyle == QLatin1String("normal")) {
-            fontStyle->setStyle(QFont::StyleNormal);
-        } else if (attributes.fontStyle == QLatin1String("italic")) {
-            fontStyle->setStyle(QFont::StyleItalic);
-        } else if (attributes.fontStyle == QLatin1String("oblique")) {
-            fontStyle->setStyle(QFont::StyleOblique);
-        }
-    }
+    if (parsedFontStyle)
+        fontStyle->setStyle(*parsedFontStyle);
 
     if (!attributes.fontWeight.isEmpty() && attributes.fontWeight != tokens::inherit) {
         bool ok = false;
